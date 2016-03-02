bcf452a03077a9f7bc10795c06b026eaaebe1309
Vibhaj Rajan <vibhaj8@gmail.com>
2016-03-02 12:22:04 +0530

    echo command

--- lib/Local.php
+++ lib/Local.php
@@ -113,10 +113,11 @@ class Local {
       fclose($this->pipes[1]);
       fclose($this->pipes[2]);
 
+      echo 'kill -15 ' . $this->pid;
       if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN')
         exec('kill -15 ' . $this->pid);
-
-      proc_terminate($this->handle);
+      else
+        proc_terminate($this->handle);
       $status = proc_get_status($this->handle);
       echo "Stopping";
       print_r($status);
