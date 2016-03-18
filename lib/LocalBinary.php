74eeb265a6a8f858d2719ebdb02cf3dd90e3c966
Harish Ved <ved.harish3@gmail.com>
2016-03-18 13:27:08 +0530

    Windows Changes

--- lib/LocalBinary.php
+++ lib/LocalBinary.php
@@ -52,7 +52,7 @@ class LocalBinary {
     if (PHP_OS == "Darwin")
       return 'https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal-darwin-x64';
     else if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
-      return 'https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal-win32.exe';
+      return 'https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal.exe';
     if ((strtoupper(PHP_OS)) == "LINUX") {
       if (PHP_INT_SIZE * 8 == 64)
         return 'https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal-linux-x64';
