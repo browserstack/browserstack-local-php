37bbcc207754d296ed933888dbc44dbaea971c62
Nidhi Makhijani <nidhimj22@gmail.com>
2016-04-25 12:30:24 +0530

    args

--- example.php
+++ example.php
@@ -11,7 +11,7 @@ use BrowserStack\LocalException;
 $me = new Local();
 $me->isRunning();
 echo "starting";
-$args = array("v" => 1);
+$args = array("-v" => 1);
 $me->start($args);
 echo "started";
 echo $me->isRunning();
