95986f929b72ac08d319ea1861b216159ca85736
Vibhaj Rajan <vibhaj8@gmail.com>
2016-04-28 02:18:49 +0530

    added dynamic args support

--- lib/Local.php
+++ lib/Local.php
@@ -18,6 +18,7 @@ class Local {
   public function __construct() {
     $this->key = getenv("BROWSERSTACK_ACCESS_KEY");
     $this->logfile = getcwd() . "/local.log";
+    $this->user_args = array();
   }
 
   public function __destruct() {
@@ -32,39 +33,45 @@ class Local {
   }
 
   public function add_args($arg_key, $value = NULL) {
-    if ($arg_key == "-key")
+    if ($arg_key == "key")
       $this->key = $value;
-    elseif ($arg_key == "-binaryPath")
+    elseif ($arg_key == "binaryPath")
       $this->binary_path = $value;
-    elseif ($arg_key == "-logfile")
+    elseif ($arg_key == "logfile")
       $this->logfile = $value;
-    elseif ($arg_key == "-v")
+    elseif ($arg_key == "v")
       $this->verbose_flag = "-vvv";
-    elseif ($arg_key == "-force")
+    elseif ($arg_key == "force")
       $this->force_flag = "-force";
-    elseif ($arg_key == "-only")
+    elseif ($arg_key == "only")
       $this->only_flag = "-only";
-    elseif ($arg_key == "-onlyAutomate")
+    elseif ($arg_key == "onlyAutomate")
       $this->only_automate_flag = "-onlyAutomate";
-    elseif ($arg_key == "-forcelocal")
+    elseif ($arg_key == "forcelocal")
       $this->force_local_flag = "-forcelocal";
-    elseif ($arg_key == "-localIdentifier")
+    elseif ($arg_key == "localIdentifier")
       $this->local_identifier_flag = "-localIdentifier $value";
-    elseif ($arg_key == "-proxyHost")
+    elseif ($arg_key == "proxyHost")
       $this->proxy_host = "-proxyHost $value";
-    elseif ($arg_key == "-proxyPort")
+    elseif ($arg_key == "proxyPort")
       $this->proxy_port = "-proxyPort $value";
-    elseif ($arg_key == "-proxyUser")
+    elseif ($arg_key == "proxyUser")
       $this->proxy_user = "-proxyUser $value";
-    elseif ($arg_key == "-proxyPass")
+    elseif ($arg_key == "proxyPass")
       $this->proxy_pass = "-proxyPass $value";
-    elseif ($arg_key == "-forceproxy")
+    elseif ($arg_key == "forceproxy")
       $this->force_proxy_flag = "-forceproxy";
-    elseif ($arg_key == "-hosts")
+    elseif ($arg_key == "hosts")
       $this->hosts = $value;
-    elseif ($arg_key == "-f") {
+    elseif ($arg_key == "f") {
       $this->folder_flag = "-f";
       $this->folder_path = $value;
+    }
+    elseif (strtolower($value) == "true"){
+      array_push($this->user_args, "-$arg_key");
+    }
+    else {
+      array_push($this->user_args, "-$arg_key '$value'");
     }
   }
 
@@ -138,7 +145,8 @@ class Local {
     if(strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
       $exec = "call";
 
-    $command = "$exec $this->binary_path -logFile $this->logfile $this->folder_flag $this->key $this->folder_path $this->force_local_flag $this->local_identifier_flag $this->only_flag $this->only_automate_flag $this->proxy_host $this->proxy_port $this->proxy_user $this->proxy_pass $this->force_proxy_flag $this->force_flag $this->verbose_flag $this->hosts";
+    $user_args = join(' ', $this->user_args);
+    $command = "$exec $this->binary_path -logFile $this->logfile $this->folder_flag $this->key $this->folder_path $this->force_local_flag $this->local_identifier_flag $this->only_flag $this->only_automate_flag $this->proxy_host $this->proxy_port $this->proxy_user $this->proxy_pass $this->force_proxy_flag $this->force_flag $this->verbose_flag $this->hosts $user_args";
     $command = preg_replace('/\s+/S', " ", $command);
     return $command;
   }
