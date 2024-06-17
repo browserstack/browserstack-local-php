760bb889c26fb0b89bc737a21ed9120a9af4f2d2
Samarjeet <samarjeet@browserstack.com>
2024-06-17 12:03:05 +0530

    Update binary url to cloudflare R2

--- lib/LocalBinary.php
+++ lib/LocalBinary.php
@@ -54,14 +54,14 @@ class LocalBinary {
 
   private function platform_url(){
     if (PHP_OS == "Darwin")
-      return 'https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal-darwin-x64';
+      return 'https://www.browserstack.com/local-testing/downloads/bin/BrowserStackLocal-darwin-x64';
     else if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
-      return 'https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal.exe';
+      return 'https://www.browserstack.com/local-testing/downloads/bin/BrowserStackLocal.exe';
     if ((strtoupper(PHP_OS)) == "LINUX") {
       if (PHP_INT_SIZE * 8 == 64)
-        return 'https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal-linux-x64';
+        return 'https://www.browserstack.com/local-testing/downloads/bin/BrowserStackLocal-linux-x64';
       else
-        return 'https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal-linux-ia32';
+        return 'https://www.browserstack.com/local-testing/downloads/bin/BrowserStackLocal-linux-ia32';
     }
   }
 
